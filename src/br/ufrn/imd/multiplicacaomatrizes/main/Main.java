a5c5df8c7761e771f1d3ee13d56707e9d2e0f45d
aroldo.felix <junioraroldo37@gmail.com>
2021-12-18 08:47:04 -0300

    Implementação - Implementando a Main.main

--- src/br/ufrn/imd/multiplicacaomatrizes/main/Main.java
+++ src/br/ufrn/imd/multiplicacaomatrizes/main/Main.java
@@ -7,20 +7,37 @@ import br.ufrn.imd.multiplicacaomatrizes.matrizes.Matriz;
 import java.util.List;
 
 public class Main {
+    private static final int MAXIMA_VEZES_EXECUCAO = 20;
+
     public static void main(String[] args) {
         Argumentos argumentos = Argumentos.of(List.of(args));
 
         Matriz matrizA = argumentos.getMatrizA();
         Matriz matrizB = argumentos.getMatrizB();
 
-        Matriz matrizC;
+        Matriz matrizC = null;
 
         TipoMultiplicacao tipoMultiplicacao = argumentos.getTipoMultiplicacao();
 
-        if (tipoMultiplicacao == TipoMultiplicacao.C) {
-            matrizC = matrizA.multiplicarConcorrente(matrizB);
-        } else {
-            matrizC = matrizA.multiplicarSequencial(matrizB);
+        for (int i = 0; i < MAXIMA_VEZES_EXECUCAO; i++) {
+            final Matriz matrizResultante;
+
+            long tempoInicial = System.currentTimeMillis();
+            if (tipoMultiplicacao == TipoMultiplicacao.C) {
+                matrizResultante = matrizA.multiplicarConcorrente(matrizB);
+            } else {
+                matrizResultante = matrizA.multiplicarSequencial(matrizB);
+            }
+            long tempoFinalSegundos = (System.currentTimeMillis() - tempoInicial);
+
+            final var matrizANome = matrizA.getNome() + matrizA.getDimensao().toString("x");
+            final var matrizBNome = matrizB.getNome() + matrizB.getDimensao().toString("x");
+
+            EscritaArquivo.registrarCSV(matrizANome, matrizBNome, i, tempoFinalSegundos);
+
+            if (matrizC == null) {
+                matrizC = matrizResultante;
+            }
         }
 
         EscritaArquivo.escrever(matrizC, tipoMultiplicacao);
